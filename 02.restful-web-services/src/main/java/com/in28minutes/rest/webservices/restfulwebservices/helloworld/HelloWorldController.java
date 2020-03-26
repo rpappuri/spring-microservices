5fc984a9b3c28a7fc881a7bc61874a987333e8dc
Rahul Pappuri <rahul.pappuri495@gmail.com>
2020-03-26 08:25:13 +0000

    Update HelloWorldController.java

--- 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ 02.restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -19,7 +19,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello World by Rahul";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
